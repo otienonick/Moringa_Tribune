0fda2850375c1e27c4a3124525e676aa35346240
otienonick <nick.odongi@student.moringaschool.com>
2021-10-04 17:45:17 +0300

    added initial files

---
+++ news/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+import datetime as dt
+
+
+
+# Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save() 
+        
+          
+    class meta:
+        ordering =['first_name']
+  
+
+class tags(models.Model):
+    name = models.CharField(max_length = 30)    
+
+    def __str__(self):
+        return self.name
+ 
+
+class Article(models.Model):
+    title = models.CharField(max_length = 60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete = models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    
+  
+#  DateTimeField to store the exact date and time our article was posted
+#  We pass in the argument auto_now_add and equate it to True. This will automatically save the exact time and date to the database as soon as we save that model.
+
+   
---
+++ news/tests.py
@@ -0,0 +1,67 @@
+from datetime import date, timedelta
+from django.test import TestCase
+from .models import Editor,Article,tags
+import datetime as dt
+
+
+# Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.nick = Editor(first_name = 'Nick', last_name ='Otieno', email ='nick@moringaschool.com')
+
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nick,Editor))  
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.nick.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)   
+
+   
+
+class ArticleTestClass(TestCase):
+
+        def setUp(self):
+ 
+            # Creating a new editor 
+            self.nick = Editor(first_name = 'Nick', last_name ='Otieno', email ='nick@moringaschool.com')
+            self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.nick , pub_date = '22-05-2015' )
+
+            # Creating a new tag 
+            self.new_tag = tags(name = 'testing')
+
+        # Testing  instance
+
+        def test_instance(self):
+            self.assertTrue(isinstance(self.new_article,Article))  
+
+        def test_save_article(self):
+            self.nick.save_editor()
+            self.new_article.save()
+            self.new_tag.save()
+
+        def tearDown(self):
+            Editor.objects.all().delete()
+            tags.objects.all().delete()
+            Article.objects.all().delete()
+
+        def test_delete_articles(self):
+            Editor.objects.all().delete()
+            tags.objects.all().delete()
+            Article.objects.all().delete()
+
+      
+        
+
+
+
+     
+
+
+   
